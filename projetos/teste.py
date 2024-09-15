d5c1f710feb79cf428d911f475b64ebbc669d425
Vinicio :) <vinicioolivindo92@gmail.com>
2024-09-15 17:37:39 -0300

    .

--- projetos/teste.py
+++ projetos/teste.py
@@ -1,6 +1,2 @@
-from uteis import numeros
-
-num = int(input('Digite um valor: '))
-fat = numeros.fatorial(num)
-print(f'O fatorial de {num} é {fat}')
-print(f'O dobro de {num} é {numeros.dobro(num)}')
+opcao = str(input())
+s
